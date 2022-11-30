c41b9cb8898e35bb7733b066bd78c67052a8ce24
CodeSandbox <support@codesandbox.io>
2022-11-30 17:00:02 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import postRoutes from "./routes/posts.js";
+
+const app = express();
+app
+  .use("/posts", postRoutes)
+  .use(bodyParser.json({ limit: "30mb", extended: true }))
+  .use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
+  .use(cors());
+
+// AeUU1mbxneDguB89
+const CONNECTION_URL =
+  "mongodb+srv://IGNACY:winter3435@cluster0.3gepkoz.mongodb.net/?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+  )
+  .catch((error) => console.log(error));
