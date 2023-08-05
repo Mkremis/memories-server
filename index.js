feaaa03aab51b6e1186676bbf856c8fde5c0e8ee
Mkremis <105755654+Mkremis@users.noreply.github.com>
2023-08-05 18:48:12 +0000

    auth

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 import postRoutes from "./routes/posts.js";
+import userRoutes from "./routes/users.js";
 import dotenv from 'dotenv'
 
 dotenv.config()
@@ -13,6 +14,7 @@ app
   .use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
   .use(cors())
   .use("/posts", postRoutes)
+  .use("/user", userRoutes)
 
 const PORT = process.env.PORT || 3500;
 mongoose
